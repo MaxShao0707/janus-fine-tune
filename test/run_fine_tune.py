c78245a48cf53238415f12cbc7d9ea269066f9d1
MaxShao0707 <1578329406@qq.com>
2025-02-20 10:10:15 +0800

    fine tune janus-pro-7b model

---
+++ test/run_fine_tune.py
@@ -0,0 +1,50 @@
+import torch
+from transformers import AutoModelForCausalLM
+from janus.models import MultiModalityCausalLM, VLChatProcessor
+from janus.utils.io import load_pil_images
+
+model_path = "/home/ubuntu/janus_finetune/output/Janus-Pro-7B/v4-20250219-170511/checkpoint-2500-merged/"
+
+image='/home/ubuntu/janus_finetune/Janus/test/test_01.jpg'
+
+question="交通灯是什么颜色？请直接用一个单词或数字回答。"
+vl_chat_processor: VLChatProcessor = VLChatProcessor.from_pretrained(model_path)
+tokenizer = vl_chat_processor.tokenizer
+
+vl_gpt: MultiModalityCausalLM = AutoModelForCausalLM.from_pretrained(
+    model_path, trust_remote_code=True
+)
+vl_gpt = vl_gpt.to(torch.bfloat16).cuda().eval()
+
+conversation = [
+    {
+        "role": "<|User|>",
+        "content": f"<image_placeholder>\n{question}",
+        "images": [image],
+    },
+    {"role": "<|Assistant|>", "content": ""},
+]
+
+# load images and prepare for inputs
+pil_images = load_pil_images(conversation)
+prepare_inputs = vl_chat_processor(
+    conversations=conversation, images=pil_images, force_batchify=True
+).to(vl_gpt.device)
+
+# # run image encoder to get the image embeddings
+inputs_embeds = vl_gpt.prepare_inputs_embeds(**prepare_inputs)
+
+# # run the model to get the response
+outputs = vl_gpt.language_model.generate(
+    inputs_embeds=inputs_embeds,
+    attention_mask=prepare_inputs.attention_mask,
+    pad_token_id=tokenizer.eos_token_id,
+    bos_token_id=tokenizer.bos_token_id,
+    eos_token_id=tokenizer.eos_token_id,
+    max_new_tokens=512,
+    do_sample=False,
+    use_cache=True,
+)
+
+answer = tokenizer.decode(outputs[0].cpu().tolist(), skip_special_tokens=True)
+print(f"{prepare_inputs['sft_format'][0]}", answer)
